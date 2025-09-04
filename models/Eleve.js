2243228ddbbcfdc77d376d0b58bf2cc804d75dd6
Mghiii <abd.mghimimi@gmail.com>
2025-09-04 09:55:28 +0100

    updates

--- models/Eleve.js
+++ models/Eleve.js
@@ -4,7 +4,12 @@ const eleveSchema = new mongoose.Schema(
   {
     nom: { type: String, required: true },
     classe: { type: mongoose.Schema.Types.ObjectId, ref: "Classe" },
-    seances: [{ type: mongoose.Schema.Types.ObjectId, ref: "Seance" }]
+    seances: [{ type: mongoose.Schema.Types.ObjectId, ref: "Seance" }],
+    imgProfil: {
+      type: String,
+      default:
+        "https://www.pngarts.com/files/10/Default-Profile-Picture-PNG-Transparent-Image.png",
+    },
   },
   { timestamps: true }
 );
